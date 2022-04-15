d9335033158de06d8b396d2c86696e624fe5d067
Subsaw <50599540+Subsaw@users.noreply.github.com>
2022-04-15 21:43:59 +0200

    Home page works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Dashboard from './components/layout/Dashboard';
+import NavBar from './components/layout/NavBar';
+import Pokemon from './components/pokemon/Pokemon';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Dashboard/>} />
+          <Route path="/pokemon/:pokemonIndex" element={<Pokemon/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NavBar extends Component {
+  render() {
+    return (
+        <div class="navbar bg-base-100">
+            <a class="btn btn-ghost normal-case text-xl">Pokedex</a>
+        </div>
+    )
+  }
+}
